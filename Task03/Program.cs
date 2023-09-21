b08360cdd673b992c0af393fb6f2bd0990ac2916
k0chan <ivan.43.dal@yandex.ru>
2023-09-21 13:08:56 +0300

    End Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите номер дня:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
+    Console.WriteLine($"{number} -> Понедельник");
+else if (number == 2)
+    Console.WriteLine($"{number} -> Вторник");
+else if (number == 3)
+    Console.WriteLine($"{number} -> Среда");
+else if (number == 4)
+    Console.WriteLine($"{number} -> Четверг");
+else if (number == 5)
+    Console.WriteLine($"{number} -> Пятница");
+else if (number == 6)
+    Console.WriteLine($"{number} -> Суббота");
+else if (number == 7)
+    Console.WriteLine($"{number} -> Воскресенье");
+else
+    Console.WriteLine($"Не правильное число!!!");
