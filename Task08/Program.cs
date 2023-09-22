6ebfe6ade2fc695910d277f72a532a5d38fc8200
k0chan <ivan.43.dal@yandex.ru>
2023-09-22 22:53:06 +0300

    End Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 2)
+{
+    int count = 2;
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count = count + 2;
+    }
+}
+else
+    Console.WriteLine("Не правильно введено число");
