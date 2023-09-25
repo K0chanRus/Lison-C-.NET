803474b029856402e6dfd54a18841dad3595f924
K0chan <ivan.43.dal@yandex.ru>
2023-09-25 13:00:34 +0300

    End Task14

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Multiple (int num)
+{
+    int firstResult = num % 7;
+    int secondResult = num % 23;
+    return firstResult + secondResult;
+}
+
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numResult = Multiple(number);
+if (numResult > 0) Console.WriteLine("Нет");
+else Console.WriteLine("Да");
