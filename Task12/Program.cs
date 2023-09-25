46ad3d1f6bf3c6909f6ddb2596a1d9826ace3840
K0chan <ivan.43.dal@yandex.ru>
2023-09-25 12:28:24 +0300

    End Task12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 1 число");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+int multiple = firstNum % secondNum;
+if (multiple == 0) Console.WriteLine($"{firstNum},{secondNum} -> кратно");
+else Console.WriteLine($"{firstNum},{secondNum} -> не кратно, остаток {multiple}");
