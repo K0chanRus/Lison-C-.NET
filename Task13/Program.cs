584ed6c5467b775a83673c2cedd64691be38e811
K0chan <ivan.43.dal@yandex.ru>
2023-09-25 15:09:03 +0300

    End Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,24 @@
+﻿int ThirdNum(int num)
+{
+    return num % 10;
+}
+
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+    Console.WriteLine("Третьей цыфры нет");
+else if (number >= 100 && number <= 999)
+{
+    int thirdNum = ThirdNum(number);
+    Console.WriteLine($"{number} -> {thirdNum}");
+}
+else
+{
+    while (number >= 999)
+    {
+        number = number / 10;
+    }
+    int thirdNum = ThirdNum(number);
+    Console.Write($"-> {thirdNum}");
+}
