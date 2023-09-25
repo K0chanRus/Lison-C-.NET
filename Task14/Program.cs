ef90e5fede8272a58252faeffdd8db85ecf24ddf
K0chan <ivan.43.dal@yandex.ru>
2023-09-25 13:20:28 +0300

    End new Task14

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,13 +1,22 @@
-﻿int Multiple (int num)
-{
-    int firstResult = num % 7;
-    int secondResult = num % 23;
-    return firstResult + secondResult;
-}
+﻿// int Multiple (int num)
+// {
+//     int firstResult = num % 7;
+//     int secondResult = num % 23;
+//     return firstResult + secondResult;
+// }
 
 Console.Write("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int numResult = Multiple(number);
-if (numResult > 0) Console.WriteLine("Нет");
-else Console.WriteLine("Да");
+// int numResult = Multiple(number);
+// if (numResult > 0) Console.WriteLine("Нет");
+// else Console.WriteLine("Да");
+
+int Multiple (int num)
+{
+    bool multiple = (num % 7 == 0) && (num % 23 == 0);
+    return multiple;
+}
+
+bool result = Multiple(number);
+Console.WriteLine(result ? "Да" : "Нет");
