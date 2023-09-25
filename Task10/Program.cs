d2605d59974378b922f501354d7fb9313c134684
K0chan <ivan.43.dal@yandex.ru>
2023-09-25 14:18:12 +0300

    End Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int SecondNumber (int num)
+{
+    int delLast = num % 10;
+    int delFirst = num / 100;
+    return (num - delLast - (delFirst * 100)) / 10;
+}
+
+Console.Write("Введите 3 значное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+int secondNumber = SecondNumber(number);
+Console.WriteLine(secondNumber);
+}
+else
+Console.WriteLine("Число указано не правильное");
