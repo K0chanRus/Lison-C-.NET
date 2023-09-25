9a24c99d3bf674a09d1557b59cb268d2b22d6cec
K0chan <ivan.43.dal@yandex.ru>
2023-09-25 13:43:04 +0300

    End Task16

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool IsOneSqeare(int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
+
+Console.Write("Введите 1 число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsOneSqeare(number1, number2);
+Console.WriteLine(result ? "Да" : "Нет");
