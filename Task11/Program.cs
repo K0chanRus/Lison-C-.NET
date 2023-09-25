12cc5b95fece2d2ceb205a5f91f633b888d6e41f
K0chan <ivan.43.dal@yandex.ru>
2023-09-25 11:30:36 +0300

    End Task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(100, 1000);
+
+int DelSecond (int num)
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    return firstDigit * 10 + lastDigit;
+}
+int delSecond = DelSecond(number);
+Console.WriteLine($"{number} -> {delSecond}");
