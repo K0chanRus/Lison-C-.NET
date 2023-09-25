afaf1bdc657f69182dab75d9a2f2380da513d523
K0chan <ivan.43.dal@yandex.ru>
2023-09-25 15:24:52 +0300

    End Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool WeekDay (int num)
+{
+    return num == 6 || num == 7;
+}
+
+Console.Write("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day > 0 && day < 8) {
+bool result = WeekDay(day);
+Console.WriteLine(result ? "Да" : "Нет");
+}
+else Console.Write("Нет такого дня недели");
