84f93487277458d78b6867e59052f4feebee1dee
K0chan <ivan.43.dal@yandex.ru>
2023-09-29 10:14:56 +0300

    End Task 18

---
+++ Task18/Program.cs
@@ -0,0 +1,18 @@
+﻿string GetCoordinateRange (int num)
+{
+    if(num == 1) return "x > 0, y > 0";
+    if(num == 2) return "x < 0, y > 0";
+    if(num == 3) return "x < 0, y < 0";
+    if(num == 4) return "x > 0, y < 0";
+    return null;
+}
+
+Console.WriteLine("Ведите номер четверти:");
+int quater = Convert.ToInt32(Console.ReadLine());
+
+string getCoordinateRange = GetCoordinateRange(quater);
+
+string result = getCoordinateRange != null
+        ? $"Заданому номеру четверти соответствует диапазон {getCoordinateRange}" 
+        : $"Введены не коректные данные";
+Console.WriteLine(result);
