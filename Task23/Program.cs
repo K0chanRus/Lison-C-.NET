99602fbf2fca830c74aab5c5aceecb5b3425f586
K0chan <ivan.43.dal@yandex.ru>
2023-10-02 01:07:56 +0300

    End Task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿void TableCube (int number)
+{
+    int count = 1;
+    while (count <= number)
+    {
+        Console.WriteLine($"{count,3} {count * count * count,7}");
+        count++;
+    }
+}
+
+Console.WriteLine ("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+TableCube(num);
