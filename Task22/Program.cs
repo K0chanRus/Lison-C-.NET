c7c05514edd8b69dc67d40d87c63beefb8ddaa50
K0chan <ivan.43.dal@yandex.ru>
2023-10-02 00:56:03 +0300

    End Task 22

---
+++ Task22/Program.cs
@@ -0,0 +1,28 @@
+﻿void TableSqare (int number)
+{
+    int count = 1;
+    while (count <= number)
+    {
+        Console.WriteLine($"{count, 3}  {count * count,5}");
+        count++;
+    }
+}
+
+
+
+// int Sqare (int number)
+// {
+//     int n = number * number;
+//     return n;
+// }
+
+Console.WriteLine ("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+//int tab = 1;
+TableSqare(num);
+// while (num >= tab)
+// {
+//     int sqare = Sqare (tab);
+//     Console.WriteLine($"{tab} {sqare}");
+//     tab++;
+// }
