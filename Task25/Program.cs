9f6eb43390436fface83b1ff2fc5feb82d238fee
K0chan <ivan.43.dal@yandex.ru>
2023-10-02 09:45:42 +0300

    End Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,21 @@
+﻿int Degree(int num1, int num2)
+{
+    int number = 1;
+    for (int i = 0; i < num2; i++)
+    {
+        number *= num1;
+    }
+    return number;
+}
+
+Console.WriteLine("Введите число А:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numB > 0)
+{
+int degree = Degree(numA, numB);
+Console.WriteLine($"{numA}, {numB} -> {degree}");
+}
+else Console.WriteLine("Степень должна быть натуральной");
