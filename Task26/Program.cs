7667237fc6f633135f009e36edd3b010b43a5aaf
K0chan <ivan.43.dal@yandex.ru>
2023-10-02 08:15:02 +0300

    End Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,17 @@
+﻿int CounutDigit(int number)
+{
+    if (number == 0) return 1;
+    int count = 0;
+    while (number != 0)
+    {
+        number = number / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число :");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = CounutDigit(num);
+Console.WriteLine($"Количество цыфр в числе: {result}");
