0c89b656c5d44d1be81efa2a0eeac7571c744f81
K0chan <ivan.43.dal@yandex.ru>
2023-10-02 08:23:24 +0300

    End Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,18 @@
+﻿int Factorial (int num)
+{
+    int res = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            res = res * i;
+        }
+    }
+    return res;
+}
+
+Console.WriteLine("Введите цлое положительное число :");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial = Factorial(number);
+Console.WriteLine($"произведение чисел от 1 до {number} = {factorial}");
