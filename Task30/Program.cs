b8ec5548e1fae783828b9a3a57b3f321d5dd387a
K0chan <ivan.43.dal@yandex.ru>
2023-10-02 09:07:31 +0300

    End Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(2);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[] arr = CreateArray(8);
+PrintArray(arr);
