ac2af86b86edbd87f86a13811e9db457efe8cd60
K0chan <ivan.43.dal@yandex.ru>
2023-10-02 07:57:46 +0300

    End Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,18 @@
+﻿int SumNumbers (int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите цлое положительное число :");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"сумма от 1 до {number} = {sumNumbers}");
