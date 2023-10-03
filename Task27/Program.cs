faacf3eb3b7d89c9441e57d4e388b7ffcb14563c
K0chan <ivan.43.dal@yandex.ru>
2023-10-03 09:08:05 +0300

    End Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿int SumNumber (int num)
+{
+    int sumNum = 0;
+    while (num > 0)
+    {
+        sumNum = sumNum + (num % 10);
+        num = num / 10;
+    }
+    return sumNum;
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sunNumber = SumNumber(number);
+Console.WriteLine(sunNumber);
