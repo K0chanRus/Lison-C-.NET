334427106b5a5b77939dbda3e94dd54e804079a8
K0chan <ivan.43.dal@yandex.ru>
2023-10-04 22:09:17 +0300

    Add Task35

---
+++ Task35/Program.cs
@@ -0,0 +1,36 @@
+﻿int CountNumbersInRange(int[] arr, int min, int max)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= min && arr[i] <= max) count++;
+    }
+    return count;
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = CreateArrayRndInt(123, -100, 100);
+PrintArray(array);
+int result = CountNumbersInRange(array, 10, 99);
+Console.WriteLine($"Количество двухзначных чисел: {result}");
