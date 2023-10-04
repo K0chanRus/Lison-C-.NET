42a4a705f03321c78ae187e8a78bf3a8c98b38ad
K0chan <ivan.43.dal@yandex.ru>
2023-10-04 21:49:08 +0300

    Add Task33

---
+++ Task33/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+bool FindNumber(int[] arr, int number)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == number) return true;
+    }
+    return false;
+}
+
+int[]array = CreateArrayRndInt(5, -10, 10);
+PrintArray(array);
+Console.Write("Введите число для поиска:");
+int num = Convert.ToInt32(Console.ReadLine());
+bool result = FindNumber(array, 4);
+Console.WriteLine(result? "Искомое число найдено" : "Искомое число отсутствует");
