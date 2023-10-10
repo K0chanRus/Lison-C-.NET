2209b38c85391a0f32178a5951c051501ba88425
K0chan <ivan.43.dal@yandex.ru>
2023-10-10 20:04:41 +0300

    And Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,18 @@
+﻿int ConvertTenInTwo(int number, int inNum)
+{
+    int result = 0;
+    int mult = 1;
+    while (number > 0)
+    {
+        result += number % inNum * mult;
+        number /= inNum;
+        mult *= 10;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите дясятичное число :");
+int num = Convert.ToInt32(Console.ReadLine());
+int inTwo = ConvertTenInTwo(num, 2);
+Console.WriteLine($"{num} -> {inTwo}");
+
