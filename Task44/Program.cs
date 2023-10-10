71da2c39fdace4ddd4c80dfd2dfce167270f2518
K0chan <ivan.43.dal@yandex.ru>
2023-10-10 20:25:26 +0300

    And Task44

---
+++ Task44/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] FiboArray(int num)
+{
+    int[] fibonacci = new int[num];
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    for (int i = 2; i < fibonacci.Length; i++)
+    {
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    }
+    return fibonacci;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+Console.WriteLine("Введите число цыфр фибоначи");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] result = FiboArray(number);
+PrintArray(result);
