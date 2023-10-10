17d52f4e1e63b1d54caa94a7a1073dbe36874fa6
K0chan <ivan.43.dal@yandex.ru>
2023-10-10 19:33:01 +0300

    And Task30

---
+++ Task40/Program.cs
@@ -0,0 +1,13 @@
+﻿bool IsExistTriangle(int a, int b, int c)
+{
+    return a < b + c && b < b + c && c < a + b;
+}
+
+Console.Write("Введите 1 сторону: ");
+int side1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 сторону: ");
+int side2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3 сторону: ");
+int side3 = Convert.ToInt32(Console.ReadLine());
+bool result = IsExistTriangle(side1, side2, side3);
+Console.WriteLine(result ? "Треугольник существует" : "Треугольник не существует");
